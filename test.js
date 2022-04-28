7e5d5cc7cbfd1d78ffc7b7758cd18d88d3ae9bd8
limsohee <limsohee0521@gmail.com>
2022-04-28 16:54:27 +0900

    연습

---
+++ test.js
@@ -0,0 +1,29 @@
+// console.log("test");
+// console.log(155433 + 45);
+// console.log(2394892038 + 12093);
+// console.log(155433 + 452093);
+// console.log(15542233 + 421425);
+
+// let one = 1290378124;
+// let two = 120471;
+// let three = 1092374190740;
+// console.log(one + two);
+// confirm.log(two + three);
+
+// 첫번째 매개변수 뭐야?
+
+
+// function sum( /*매개 변수, parameter, param, 인자 */front,back){
+
+//   return front + back;
+//   // 값은 모르겠고 어쨌든 front 와 back을 더해 = 덧셈하는 기계
+// }
+// // sum(1,2);
+// console.log(sum(/* argument, 인수 */ 1 , 2 ));
+
+
+function sum (front,back){
+  console.log(front+back); // console.log 찍는 용도의 기계
+}
+
+sum(2,3)
